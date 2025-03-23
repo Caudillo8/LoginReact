02d490bebc6e775dc792c5a806d52825f193e133
hernandez matias <matias04hernandez5329@gmail.com>
2025-03-23 18:41:31 -0300

    css

--- login-react/src/components/Form/MiLogin.jsx
+++ login-react/src/components/Form/MiLogin.jsx
@@ -1,29 +1,58 @@
-export const MiLogin=()=>{/*Siempre Pascal Case!!!*/ 
-    return(
-      <div className="ProductCard">
-        <h1>Crea tu cuenta</h1>
-        <h4>Ingresa tu información en el registro.</h4>
-        <form>
-          <label>Nombre</label>
-          <input type="text" placeholder="nombre"/>
-      
-          <label>Apellido</label>
-          <input type="text" placeholder="apellido"/><br /><br />
-  
-          <label>Email</label><br />
-          <input type="email" placeholder="correo electrónico"/><br /><br />
-  
-          <label>Teléfono</label><br />
-          <input type="tel" placeholder="correo electrónico"/><br /><br />
-  
-          <label>Password</label><br />
-          <input type="password" placeholder="contraseña"/><br /><br />
-  
-          <label>Confirma pass</label><br />
-          <input type="password" placeholder="confirma contraseña"/><br /><br />
-  
-          <input type="submit" value="Registrarse" />
-  
-        </form>
-      </div>
-    )};
+import "./MiLogin.css";
+import { Icons } from "../Icons/Eye.jsx";
+export const MiLogin = () => {
+  /*Siempre Pascal Case!!!*/
+  return (
+    <div className="container">
+      <h1>Crea tu cuenta</h1>
+      <h4 className="subtitulo">Ingresa tu información en el registro.</h4>
+      <form>
+        <div className="nombreApellido">
+          <div className="datoNombre">
+            <label>Nombre</label>
+            <input type="text" placeholder="nombre" />
+          </div>
+          <div className="datoNombre">
+            <label>Apellido</label>
+            <input type="text" placeholder="apellido" />
+          </div>
+          <br />
+          <br />
+        </div>
+        <div className="datoLargo">
+          <label>Email</label>
+          <br />
+          <input type="email" placeholder="correo electrónico" />
+          <br />
+          <br />
+        </div>
+        <div className="datoLargo">
+          <label>Teléfono</label>
+          <br />
+          <input type="tel" placeholder="correo electrónico" />
+          <br />
+          <br />
+        </div>
+        <div className="datoLargo">
+          <label>Password</label>
+          <br />
+          <div className="password-input">
+            <input type="password" placeholder="contraseña" />
+            <Icons.EyePassword></Icons.EyePassword>
+            <br />
+            <br />
+          </div>
+          <div className="div datoLargo">
+            <label>Confirma pass</label>
+            <br />
+            <input type="password" placeholder="confirma contraseña" />
+            <Icons.EyePassword></Icons.EyePassword>
+            <br />
+            <br />
+          </div>
+        </div>
+        <input type="submit" value="Registrarse" className="btn" />
+      </form>
+    </div>
+  );
+};
